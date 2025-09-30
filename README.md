e45835812f5c1fb4f15c85d2aba41ece77b1d36a
lotaway <576696294@qq.com>
2025-09-30 16:38:05 +0800

    Fix error in model load.

--- README.md
+++ README.md
@@ -15,3 +15,55 @@ Install Vscode Continue Plugin and connect to the server.
 ## AMD Required
 
 Need install ROCm and [torch ROCm version](https://rocm.docs.amd.com/projects/radeon-ryzen/en/latest/docs/install/installrad/wsl/install-pytorch.html) in Linux/WSL with AMD GPU.
+
+## About bitsandbytes
+
+Official documents clearly state that AMD GPUs must be compiled from source:
+
+```bash
+git clone https://github.com/bitsandbytes-foundation/bitsandbytes.git
+cd bitsandbytes
+
+# Switch to the version you installed, such as 0.47.0
+git fetch origin rocm-build-update:rocm-build-update
+git switch rocm-build-update
+
+# Setup ROCm environment variables
+export BNB_ROCM_ARCH=1
+export ROCM_HOME=/opt/rocm
+export PATH=$ROCM_HOME/bin:$PATH
+export HIP_PATH=$ROCM_HOME
+
+# compile
+cmake -DCOMPUTE_BACKEND=hip -DBNB_ROCM_ARCH="gfx90a;gfx942;gfx1100" .
+cmake --build .
+```
+
+If got error like:
+
+```bash
+CMake Error at /opt/rocm/lib/cmake/hsa-runtime64/hsa-runtime64Targets.cmake:80 (message):
+  The imported target "hsa-runtime64::hsa-runtime64" references the file
+
+     "/opt/rocm/lib/libhsa-runtime64.so.1.15.60400"
+
+  but this file does not exist.
+```
+
+Follow [Issue: cmake hsa-runtime64 in wsl2 not correctly set the library](https://github.com/ROCm/ROCm/issues/3606) to fix it with:
+
+```bash
+cd /opt/rocm/lib/
+ln -s libhsa-runtime64.so.1.14 libhsa-runtime64.so.1.15.60400
+```
+
+After build success, you will see:
+```bash
+[100%] Linking CXX shared library bitsandbytes/libbitsandbytes_rocm64.so
+[100%] Built target bitsandbytes
+```
+
+Take `libbitsandbytes_rocm64.so` file to `/home/{user}/miniforge3/envs/python{version}/lib/python{version}/site-packages/bitsandbytes` directory.
+Notice: I use miniforge3 as python environment here, if you use other python environment, you should change the directory accordingly. Such as if you're using `anaconda`, directory prefix are `/home/{user}/canda/`
+
+Done all process, now you can use `bitsandbytes` in your python code.
--- model_provider.py
+++ model_provider.py
@@ -1,26 +1,53 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, AutoConfig
-from accelerate import init_empty_weights
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    AutoConfig,
+)
+from accelerate import init_empty_weights, infer_auto_device_map
 import torch
 
+
 class LocalModel:
-    def __init__(self, model_name="facebook/galactica-30b"):
-        tokenizer_model_name = "Xenova/claude-tokenizer"
+    def __init__(self, model_name="./models/deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"):
+        tokenizer_model_name = "./models/deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
 
         # config = AutoConfig.from_pretrained(model_name)
         # with init_empty_weights():
         #     model = AutoModelForCausalLM.from_config(config)
 
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            tokenizer_model_name, local_files_only=True
+        )
         quantization_config = BitsAndBytesConfig(
             # load_in_8bit=True,
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.float16
+            bnb_4bit_compute_dtype=torch.float16,
+            llm_int8_enable_fp32_cpu_offload=True,
         )
+        # device_map = infer_auto_device_map(
+        #     self.model,
+        #     max_memory={0: "20GiB", "cpu": "60GiB"},
+        #     no_split_module_classes=["DeepseekLayer"],  # follow real model
+        # )
         self.model = AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype=torch.float16, device_map="auto", quantization_config=quantization_config,
+            model_name,
+            local_files_only=True,
+            dtype=torch.float16,
+            device_map={
+                "transformer.word_embeddings": 0,
+                "transformer.final_layernorm": 0,
+                "transformer.h": "cpu",
+                "model.embed_tokens": 0,  # 明确分配嵌入层到GPU0
+                "model.layers": "cpu",  # 中间层分配到CPU
+                "model.norm": 0,  # 最后的LayerNorm留在GPU
+                "lm_head": 0,  # 输出层留在GPU
+            },
+            # device_map=device_map,
+            quantization_config=quantization_config,
+            max_memory={0: "20GiB", "cpu": "60GiB"},
         )
-
 
     def chat(self, messages: list[dict]) -> str:
         if hasattr(self.tokenizer, "apply_chat_template"):
