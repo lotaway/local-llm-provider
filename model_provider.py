5e45511e21d28f63ccd56a816ebcf2edadf0a12d
lotaway <576696294@qq.com>
2025-11-23 23:16:10 +0800

    Remove quantization in mac system.

--- model_provider.py
+++ model_provider.py
@@ -84,12 +84,13 @@ class LocalLLModel:
                 low_cpu_mem_usage=True,
             )
         else:
+            is_mac = os.sys.platform == "darwin"
             quantization_config = BitsAndBytesConfig(
                 load_in_4bit=True,
                 bnb_4bit_quant_type="nf4",
                 bnb_4bit_compute_dtype=torch.float16,
                 llm_int8_enable_fp32_cpu_offload=True,
-            )
+            ) if is_mac is False else None
             self.model = AutoModelForCausalLM.from_pretrained(
                 model_path,
                 local_files_only=True,
