f8f86b6a23329543e6e0f13a58caff12f4919395
lotaway <576696294@qq.com>
2025-11-26 22:38:52 +0800

    Update milvus dependency.

--- rag.py
+++ rag.py
@@ -15,7 +15,7 @@ from langchain_community.document_loaders import (
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 # from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Milvus
+from langchain_milvus import Milvus
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
