e89730ab8291d25dbe58a942cfa56759e53a67b3
lotaway <576696294@qq.com>
2025-12-07 23:20:38 +0800

    Fix es retriever error.

--- retrievers/__init__.py
+++ retrievers/__init__.py
@@ -3,5 +3,6 @@
 from .hybrid_retriever import HybridRetriever
 from .reranker import Reranker
 from .expanded_retriever import ExpandedRetriever
+from .es_retriever import ESBM25Retriever
 
-__all__ = ["HybridRetriever", "Reranker", "ExpandedRetriever"]
+__all__ = ["HybridRetriever", "Reranker", "ExpandedRetriever", "ESBM25Retriever"]
