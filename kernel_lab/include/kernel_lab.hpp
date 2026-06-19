91cdd3eb773ba3450bc6f98ccd0e75e3d009617d
lotaway <576696294@qq.com>
2026-06-19 23:39:04 +0800

    Update kernel error output.

--- kernel_lab/include/kernel_lab.hpp
+++ kernel_lab/include/kernel_lab.hpp
@@ -6,7 +6,9 @@ namespace kernel_lab {
 
 void launch_add(float *a, float *b, float *c, int n, int block_size);
 
-void checkHipError(hipError_t err);
+// void checkHipError(hipError_t);
+
+void checkHipError(hipError_t, const char[], int);
 
 void performance(hipEvent_t, hipEvent_t);
 
